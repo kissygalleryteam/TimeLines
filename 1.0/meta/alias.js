9c9f4cae77cd9073ffc93ad63d23eb09abe7921f
kissyteam <tiehang.lth@taobao.com>
2015-04-11 13:47:33 +0800

    sync daily branch 3.0.0

--- 1.0/meta/alias.js
+++
@@ -1,3 +0,0 @@
-config({
-    'gallery/TimeLines/index': {alias: ['gallery/TimeLines/1.0/index']}
-});
--- 3.0.1/build/lib/main-min.js
+++
@@ -1,1 +0,0 @@
-KISSY.add("kg/timelines/3.0.1/lib/main",["node","base"],function(e,i,n,a){var t=(i("node").all,i("base"));a.exports=t.extend({initializer:function(){}},{ATTRS:{}})});
